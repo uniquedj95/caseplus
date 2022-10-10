eaee714b77ebe8321ed3e7e08e5192572a2695ed
Daniel Justin <danieljmuleke@gmail.com>
2022-10-10 13:27:16 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *  A function that convert an underscore cased string to Pascal Cased string
+ * 
+ * @param str an underscore cased string. e.g. my_variable
+ * @returns a Pascal cased string e.g MyVariable
+ */
+ export function underscoreToPascal(str: string): string {
+  return str.split('_').reduce((str, word) => str + word.charAt(0).toUpperCase() + word.slice(1), '')
+}
+
+/**
+ *  A function that convert an camelCased string to underscore_cased string
+ * 
+ * @param str a Camel cased string e.g myVariablean 
+ * @returns an underscore cased string. e.g. my_variable
+ */
+export function camelToUnderscore(str: string): string {
+  return str.split(/(?=[A-Z])/).join('_').toLowerCase()
+}
