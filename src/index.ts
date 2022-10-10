5536ffe5e94d5a997b1e1f2b546461544036822e
Daniel Justin <danieljmuleke@gmail.com>
2022-10-10 14:06:06 +0200

    [Refactor] Improve formatting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 /**
  *  A function that convert an underscore cased string to Pascal Cased string
- * 
+ *
  * @param {String} str an underscore cased string. e.g. my_variable
  * @returns a Pascal cased string e.g MyVariable
  */
- export function underscoreToPascal(str: string): string {
-  return str.split('_').reduce((str, word) => str + word.charAt(0).toUpperCase() + word.slice(1), '')
+export function underscoreToPascal(str: string): string {
+  return str
+    .split('_')
+    .reduce((str, word) => str + word.charAt(0).toUpperCase() + word.slice(1), '');
 }
 
 /**
  *  A function that convert an camelCased string to underscore_cased string
- * 
- * @param {String} str a Camel cased string e.g myVariablean 
+ *
+ * @param {String} str a Camel cased string e.g myVariablean
  * @returns an underscore cased string. e.g. my_variable
  */
 export function camelToUnderscore(str: string): string {
-  return str.split(/(?=[A-Z])/).join('_').toLowerCase()
+  return str
+    .split(/(?=[A-Z])/)
+    .join('_')
+    .toLowerCase();
 }
