3e0de17f6d210460be563220cc977e5c8cd62656
Daniel Justin <danieljmuleke@gmail.com>
2022-10-10 14:58:53 +0200

    Added test cases

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -4,10 +4,22 @@ describe("underscoreToPascal", ()=> {
   test("Convert underscore cased string to pascal cased string", () => {
     expect(underscoreToPascal("my_awesome_variable")).toBe("MyAwesomeVariable")
   })
+
+  test("Return empty string when passed string is empty", () => {
+    expect(underscoreToPascal("")).toBe("")
+  })
+
+  test("Trim/Remove excess underscores", () => {
+    expect(underscoreToPascal("_my__Variable")).toBe("MyVariable")
+  })
 })
 
 describe("camelToUnderscore", ()=> {  
   test("Convert camel cased string to underscore cased string", () => {
     expect(camelToUnderscore("myAwesomeVariable")).toBe("my_awesome_variable")
+  })
+
+  test("Return empty string when passed string is empty", () => {
+    expect(underscoreToPascal("")).toBe("")
   })
 })
